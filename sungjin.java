4aeeb9133980b2551b52fa03b1b2e52a309ace06
Sungjin Lee <sungjin8191@gmail.com>
2019-04-13 23:38:50 +0900

    sungjin.java

--- sungjin.java
+++ sungjin.java
@@ -1,9 +1,15 @@
 public class Sungjin{
-	public static void main(String[] args){
-		int a= 1;
-		for(int i=0;i<100;i++){
-			System.out.printf("%d",i);}
-		return 0;
+	
+public static void main(String[] args){
+		
+int a= 1;
+		
+for(int i=0;i<100;i++){
+			
+a+=i;
+System.out.printf("%d",a);}
+		
+return 0;
 	}
 }
 		
